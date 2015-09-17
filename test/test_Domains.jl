0ac8a104a2a34029ac0bc40ae5be601d778cfaa0
Erik Schnetter <schnetter@gmail.com>
2015-09-16 23:42:24 -0400

    More tests for BoxDomain

--- test/test_Domains.jl
+++ test/test_Domains.jl
@@ -26,6 +26,9 @@ function test_Region{V}(::Type{V})
     reg2 = Reg(p0, p2)
     @test !isempty(reg1)
     @test !isempty(reg2)
+    @test p0 ∈ reg1
+    @test p0 ∈ reg2
+    @test p1 ∈ reg2
     @test (reg1 ≠ reg2) == (D>0)
     @test (reg1 ⊊ reg2) == (D>0)
     @test reg1 ⊆ reg2
@@ -39,6 +42,8 @@ function test_Regions()
     test_Region(MultiProductVS{NTuple{4,Float64}})
 end
 test_Regions()
+
+
 
 function test_EmptyDomain{V}(::Type{V})
     @test istrait(AbstractVS{V})
@@ -64,8 +69,38 @@ test_EmptyDomains()
 
 function test_BoxDomain{V}(::Type{V})
     @test istrait(AbstractVS{V})
+    Reg = Region{V}
     Dom = BoxDomain{V}
     @test istrait(AbstractDomain{Dom})
+
+    S = veltype(V)
+    s1 = sconst(S, 1)
+    s2 = sconst(S, 2)
+    D = vdim(V)
+    @test D>=0
+    p0 = vnull(V)
+    p1 = map(x->x+s1, p0)::V
+    p2 = map(x->x+s2, p0)::V
+
+    reg0 = Reg(p0, p0)
+    reg1 = Reg(p0, p1)
+    reg2 = Reg(p0, p2)
+
+    dom0 = Dom(reg0)
+    @test vspace(Dom) === V
+    @test isempty(dom0) == isempty(reg0)
+    @test (p0 ∉ dom0) == (D>0)
+    @test boundingbox(dom0) == reg0
+
+    dom1 = Dom(reg1)
+    dom2 = Dom(reg2)
+    @test !isempty(dom1)
+    @test !isempty(dom2)
+    @test p0 ∈ dom1
+    @test p0 ∈ dom2
+    @test p1 ∈ dom2
+    @test boundingbox(dom1) == reg1
+    @test boundingbox(dom2) == reg2
 end
 function test_BoxDomains()
     test_BoxDomain(EmptyVS{Int8})
